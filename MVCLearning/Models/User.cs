0fcb814cff52f8c8ff9cd0d4ed77d054a714f8e1
Luke Walz <Luke_Walz@gallup.com>
2018-08-29 17:23:23 -0500

    Added range selecctor

---
+++ MVCLearning/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLearning.Models
+{
+    public class User
+    {
+        public int NumberOfRepeats { get; set; }
+    }
+}
--- MVCLearning/Scripts/script.jsx
+++ MVCLearning/Scripts/script.jsx
@@ -1,14 +1,79 @@
-﻿class CommentBox extends React.Component {
+﻿class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            repeat: [0],
+            name: '',
+        };
+
+        this.handleRepeatChange = this.handleRepeatChange.bind(this);
+        this.repeatEvent = this.repeatEvent.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.changeName = this.changeName.bind(this);
+        this.handle = this.handleError.bind(this);
+    }
+
+    handleRepeatChange(e) {
+        const num = e.target.value;
+        this.repeatEvent(num);
+    }
+
+    handleNameChange(e) {
+        const name = e.target.value;
+        this.changeName(name);
+    }
+
+    handleError(e) {
+        const error = e.target.value;
+        alert(error);
+    }
+
+    repeatEvent(newNumber) {
+        if (newNumber < this.state.repeat[this.state.repeat.length - 1]) {
+            this.setState({ repeat: [...this.state.repeat.splice(0, newNumber)] });
+        }
+        else {
+            this.setState({ repeat: [...this.state.repeat, newNumber] });
+        }
+    }
+
+    changeName(newName) {
+        this.setState({ name: newName });
+    }
+
     render() {
         return (
             <div>
-                {this.props.name}
+                <form>
+                    <label>
+                        Name:
+                        <input type="text" maxLength={40} value={this.state.name} onChange={this.handleNameChange} />
+                    </label>
+
+                    <br />
+
+                    <label>
+                        Repeat:
+                        <input type="range" min={0} max={7} value={this.state.repeat[this.state.repeat.length - 1]} onChange={this.handleRepeatChange} /> {this.state.repeat[this.state.repeat.length - 1]}
+                    </label>
+                </form>
+
+                {this.state.repeat.map((i) => <div key={i}>{this.state.name}</div>)}
+
             </div>
         );
     }
 }
 
+class Parent extends React.Component {
+    render() {
+        return (
+            <Form />
+        )
+    }
+}
+
 ReactDOM.render(
-    <CommentBox name='I am a prop in a react world' />,
+    <Parent />,
     document.getElementById('reactIsh')
 );
