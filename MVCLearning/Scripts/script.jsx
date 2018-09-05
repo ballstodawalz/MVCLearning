d36f9eb363e83d44fb2a79e233639720d1c8aa1e
Luke Walz <Luke_Walz@gallup.com>
2018-09-05 17:45:50 -0500

    this

--- MVCLearning/Scripts/script.jsx
+++ MVCLearning/Scripts/script.jsx
@@ -43,9 +43,6 @@
 
     componentDidMount() {
         fetch('https://api.sportradar.us/ncaafb-t1/2018/REG/2/schedule.json?api_key=tmgx6wjpu45uzjn732ke4swx', {
-            headers: new Headers({
-                'X-Originating-IP':'198.175.141.60'
-            })
         })
             .then(results => {
                 console.log(results.json())
