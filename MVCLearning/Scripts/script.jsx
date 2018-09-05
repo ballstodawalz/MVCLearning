1985418e10ea9d4826655ad12d41860a1db56cb1
Luke Walz <Luke_Walz@gallup.com>
2018-09-05 17:37:12 -0500

    maybe this will work

--- MVCLearning/Scripts/script.jsx
+++ MVCLearning/Scripts/script.jsx
@@ -44,7 +44,7 @@
     componentDidMount() {
         fetch('http://api.sportradar.us/ncaafb-t1/2018/REG/2/schedule.json?api_key=tmgx6wjpu45uzjn732ke4swx', {
             headers: new Headers({
-                'X-Originating - IP': '198.175.141.60'
+                'X-Originating-IP':'198.175.141.60'
             })
         })
             .then(results => {
